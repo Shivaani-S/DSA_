baaeed1f9687d23b202a10e3874257be381f3eb5
Shivaani S <shivaanisu@gmail.com>
2026-09-26 22:42:54 +0530

    Time: 11 ms (62.12%) | Memory: 46.2 MB (85.18%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> st = new Stack<>();
+        int result = 0;
+        int num = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            } else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num;
+                num = 0;
+                result *= st.pop();
+                result += st.pop();
+            }
+        }
+
+        return result + sign * num;
+    }
+}
